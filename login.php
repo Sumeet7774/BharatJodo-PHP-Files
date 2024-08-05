e6871f0340f8e121118923b1c081c7f78932cc6b
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-08-05 12:26:01 +0530

    created login api using prepared statement

--- login.php
+++ login.php
@@ -2,16 +2,26 @@
 
     require 'connection.php';
 
-    $email_id = $_POST['$email_id'];
-    $phone_number = $_POST['$phone_number'];
+    $email_id = $_POST['email_id'];
+    $phone_number = $_POST['phone_number'];
 
-    
+    $check_sql = "select * from users WHERE email_id = ? AND phone_number = ?";
+    $stmt = $conn->prepare($check_sql);
+    $stmt->bind_param("ss", $email_id, $phone_number);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
+    if ($result->num_rows > 0) 
+    {
+        $response = array('status' => 'found', 'message' => 'user found');
+        echo json_encode($response);
+    } 
+    else 
+    {
+        $response = array('status' => 'not found', 'message' => 'user not found');
+        echo json_encode($response);
+    }
 
-
-
-
-
-
-
+    $stmt->close();
+    $conn->close();
 ?>
