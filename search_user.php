399aacc3bac5ec5dd9f30baa37b5e8caa123dba4
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-08-17 02:24:58 +0530

    created a new api for searching the user

---
+++ search_user.php
@@ -0,0 +1,26 @@
+<?php
+
+    require 'connection.php';
+
+    $username = $_POST['username'];
+
+    $check_sql = "select * from users WHERE username = ?";
+    $stmt = $conn->prepare($check_sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) 
+    {
+        $response = array('status' => 'found', 'message' => 'user found');
+        echo json_encode($response);
+    } 
+    else 
+    {
+        $response = array('status' => 'not found', 'message' => 'user not found');
+        echo json_encode($response);
+    }
+
+    $stmt->close();
+    $conn->close();
+?>
