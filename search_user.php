1d13e2930e863c24b8b7c28c47da214422b10809
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-08-22 07:24:52 +0530

    made some changes

--- search_user.php
+++ search_user.php
@@ -4,17 +4,24 @@
 
     $username = $_POST['username'];
 
-    $check_sql = "select * from users WHERE username = ?";
+    $check_sql = "select username, phone_number from users WHERE username = ?";
     $stmt = $conn->prepare($check_sql);
     $stmt->bind_param("s", $username);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) 
-    {
-        $response = array('status' => 'found', 'message' => 'user found');
-        echo json_encode($response);
-    } 
+{
+    $row = $result->fetch_assoc();
+    $response = array(
+        'status' => 'found', 
+        'message' => 'user found',
+        'user_id' => $row['user_id'],
+        'username' => $row['username'],
+        'phone_number' => $row['phone_number']
+    );
+    echo json_encode($response);
+} 
     else 
     {
         $response = array('status' => 'not found', 'message' => 'user not found');
