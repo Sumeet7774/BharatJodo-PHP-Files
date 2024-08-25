805c80fa8f873b8790fc358e610cabbd2416a4d4
Sumeet7774 <sumeetkapadia7@gmail.com>
2024-08-26 02:33:31 +0530

    made an API for retrieving pending friend requests

--- retrieve_pending_friendrequest.php
+++ retrieve_pending_friendrequest.php
@@ -4,14 +4,18 @@
 
     $user_id = $_POST['user_id'];               //id of the user who received friend requests for which we want to fetch the pending friend requests
 
-    $query = "select * from friendhip where friend_id='$user_id' and status='pending'";
+    //$query = "select * from friendhip where friend_id='$user_id' and status='pending'";
+    $query="select u.username, u.phone_number 
+            from friendship f
+            join users u on f.user_id=u.user_id
+            where f.friend_id='$user_id' and f.status = 'pending'";
+    
     $result = $conn->query($query);
     
     if($result->num_rows > 0)
     {
         $pendingRequests = array();
 
-        // Fetch each pending friend request
         while ($row = $result->fetch_assoc()) 
         {
             $pendingRequests[] = $row;
